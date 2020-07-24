b3cfd2c04b051af882da73889bddb02bb8f050c7
Mihai Tinca <tinca_c_mihai@yahoo.com>
2020-07-24 11:31:49 +0300

    EA html page: add select option tag for choosing EA list

--- design/app.js
+++ design/app.js
@@ -1,5 +1,7 @@
 const { ipcRenderer, shell } = require('electron');
 
+
+//// Minimize and close buttons
 document.getElementById('window__btn--close').addEventListener('click', () => {
     let evResp = ipcRenderer.send('ev:close');
 });
@@ -7,3 +9,12 @@ document.getElementById('window__btn--close').addEventListener('click', () => {
 document.getElementById('window__btn--minimize').addEventListener('click', () => {
     let evResp = ipcRenderer.send('ev:minimize');
 });
+
+
+//// Get MT4 folder path
+
+
+//// Display EA options
+
+
+//// Get selected EA option
