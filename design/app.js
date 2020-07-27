3b5feaa2e7227b9f236d0d618324b63964f1938e
Melcoo <tinca_c_mihai@yahoo.com>
2020-07-27 10:14:14 +0300

    Get MT4 path and display it in js

--- design/app.js
+++ design/app.js
@@ -1,4 +1,9 @@
 const { ipcRenderer, shell } = require('electron');
+let mt4Path = '';
+
+
+// DOM elements
+
 
 
 //// Minimize and close buttons
@@ -12,9 +17,23 @@ document.getElementById('window__btn--minimize').addEventListener('click', () =>
 
 
 //// Get MT4 folder path
+document.getElementById('ea__mt4path--file').onchange = (() => {
+    const terminalPath = document.getElementById('ea__mt4path--file').files[0].path;
 
+    // Update text area with area
+    if(terminalPath.substr(-12) == 'terminal.exe') {
+        document.getElementById('ea__mt4path--path').value = terminalPath;
+        mt4Path = terminalPath.replace('terminal.exe', '');
+    } else {
+        document.getElementById('ea__mt4path--path').value = 'Path to "terminal.exe" is not correct!';
+    }
+    
+}); 
 
 //// Display EA options
 
 
 //// Get selected EA option
+
+
+//// Change opacity of "Continue" button
